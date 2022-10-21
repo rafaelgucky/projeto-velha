33129f302479379b63f04a792d8161a9324de4b7
rafaelgucky <rafaelgucky97@gmail.com>
2022-10-21 19:50:16 -0300

    fazendo

---
+++ velha.js
@@ -0,0 +1,145 @@
+var vez = 1
+var clicouq1 = false
+var clicouq2 = false
+var clicouq3 = false
+var clicouq4 = false
+var clicouq5 = false
+var clicouq6 = false
+var clicouq7 = false
+var clicouq8 = false
+var clicouq9 = false
+function velha1(){
+    var quadrado1 = document.getElementById('q1')
+    if(clicouq1 == false){
+        vez += 1
+        if(vez % 2 == 0){
+        quadrado1.style.backgroundImage = 'url(X.png)'
+        quadrado1.style.backgroundRepeat = 'no-repeat'
+        }
+        else{
+            quadrado1.style.backgroundImage = 'url(O.png)'
+            quadrado1.style.backgroundRepeat = 'no-repeat'
+        }
+        clicouq1 = true
+    }
+}
+function velha2(){
+    var quadrado2 = document.getElementById('q2')
+    if(clicouq2 == false){
+        vez += 1
+        if(vez % 2 == 0){
+            quadrado2.style.backgroundImage = 'url(X.png)'
+            quadrado2.style.backgroundRepeat = 'no-repeat'
+            }
+        else{
+            quadrado2.style.backgroundImage = 'url(O.png)'
+            quadrado2.style.backgroundRepeat = 'no-repeat'
+        }
+        clicouq2 = true
+    }
+}
+function velha3(){
+    var quadrado3 = document.getElementById('q3')
+    if(clicouq3 == false){
+        vez += 1
+        if(vez % 2 == 0){
+            quadrado3.style.backgroundImage = 'url(X.png)'
+            quadrado3.style.backgroundRepeat = 'no-repeat'
+            }
+        else{
+            quadrado3.style.backgroundImage = 'url(O.png)'
+            quadrado3.style.backgroundRepeat = 'no-repeat'
+        }
+        clicouq3 = true
+    }
+}
+function velha4(){
+    var quadrado4 = document.getElementById('q4')
+    if(clicouq4 == false){
+        vez += 1
+        if(vez % 2 == 0){
+            quadrado4.style.backgroundImage = 'url(X.png)'
+            quadrado4.style.backgroundRepeat = 'no-repeat'
+            }
+        else{
+            quadrado4.style.backgroundImage = 'url(O.png)'
+            quadrado4.style.backgroundRepeat = 'no-repeat'
+        }
+        clicouq4 = true
+    }
+}
+function velha5(){
+    var quadrado5 = document.getElementById('q5')
+    if(clicouq5 == false){
+        vez += 1
+        if(vez % 2 == 0){
+            quadrado5.style.backgroundImage = 'url(X.png)'
+            quadrado5.style.backgroundRepeat = 'no-repeat'
+            }
+        else{
+            quadrado5.style.backgroundImage = 'url(O.png)'
+            quadrado5.style.backgroundRepeat = 'no-repeat'
+        }
+        clicouq5 = true
+    }
+}
+function velha6(){
+    var quadrado6 = document.getElementById('q6')
+    if(clicouq6 == false){
+        vez += 1
+        if(vez % 2 == 0){
+            quadrado6.style.backgroundImage = 'url(X.png)'
+            quadrado6.style.backgroundRepeat = 'no-repeat'
+            }
+        else{
+            quadrado6.style.backgroundImage = 'url(O.png)'
+            quadrado6.style.backgroundRepeat = 'no-repeat'
+        }
+        clicouq6 = true
+    }
+}
+function velha7(){
+    var quadrado7 = document.getElementById('q7')
+    if(clicouq7 == false){
+        vez += 1
+        if(vez % 2 == 0){
+            quadrado7.style.backgroundImage = 'url(X.png)'
+            quadrado7.style.backgroundRepeat = 'no-repeat'
+            }
+        else{
+            quadrado7.style.backgroundImage = 'url(O.png)'
+            quadrado7.style.backgroundRepeat = 'no-repeat'
+        }
+        clicouq7 = true
+    }
+}
+function velha8(){
+    var quadrado8 = document.getElementById('q8')
+    if(clicouq8 == false){
+        vez += 1
+        if(vez % 2 == 0){
+            quadrado8.style.backgroundImage = 'url(X.png)'
+            quadrado8.style.backgroundRepeat = 'no-repeat'
+            }
+        else{
+            quadrado8.style.backgroundImage = 'url(O.png)'
+            quadrado8.style.backgroundRepeat = 'no-repeat'
+        }
+        clicouq8 = true
+    }
+}
+function velha9(){
+    var quadrado9 = document.getElementById('q9')
+    if(clicouq9 == false){
+        vez += 1
+        if(vez % 2 == 0){
+            quadrado9.style.backgroundImage = 'url(X.png)'
+            quadrado9.style.backgroundRepeat = 'no-repeat'
+            }
+        else{
+            quadrado9.style.backgroundImage = 'url(O.png)'
+            quadrado9.style.backgroundRepeat = 'no-repeat'
+        }
+        clicouq9 = true
+    }
+}
